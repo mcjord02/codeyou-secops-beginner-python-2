43c19a6ff549a11fa5275481a207d3b9bc3a3f02
mcjord02 <https://github.com/mcjord02/My-New-Repo.git>
2026-03-30 14:59:02 -0400

    Updated week2_variables

---
+++ week2_variables/asset_tagger.py
@@ -0,0 +1,10 @@
+# Week 2 – Working with Variables
+# Author: <Your Name>
+
+analyst_name = "Jordan"
+system_name = "workstation-01"
+criticality = "high"
+
+print("Analyst:", analyst_name)
+print("System:", system_name)
+print("Criticality Level:", criticality)
