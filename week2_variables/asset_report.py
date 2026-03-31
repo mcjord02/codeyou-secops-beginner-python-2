de909a705bb1f3617db49212a13ba3e7567f3d06
Mark Jordan <mcjordan3049@outlook.com>
2026-03-30 21:24:29 -0400

    Corrected asset_report script

--- week2_variables/asset_report.py
+++ week2_variables/asset_report.py
@@ -1,21 +1,25 @@
 # Asset Report Generator
+analyst_name = input("Enter name: ")
+department_name = input("Enter Department: ")
+system_hostname = input("Enter Hostname: ")
+criticality = input(" Enter Critical Score: ")
+risk_score = input("Enter a risk score (1-10): ")
+risk_score = int(risk_score)
+
 print("=================================")
 print(f"Cyber Defense - Asset Report")
 print("=================================")
 # Week 2 – Working with Variables
 # Author: <Mark Jordan>
-analyst_name = "Mark Jordan"
-department_name = "IT Security"
-system_hostname = "SecurityServer01"
-criticality = "high"
 
 print("Analyst:", analyst_name)
 print("System:", department_name)
 print("System Hostname:", system_hostname)
 print("Criticality Level:", criticality)
+print("Risk Score: ", risk_score)
 
-risk_score = input("Enter a risk score (1-10): ")
-risk_score = int(risk_score)
+
+
 
 if risk_score >= 8:
     print("[*] High-Risk Asset – prioritize review.")
